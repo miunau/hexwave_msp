32c1f198d16d2449e17b4af8cc35882c4dce56ca
miunau <55932282+miunau@users.noreply.github.com>
2021-08-02 05:22:52 +0200

    Add files via upload

--- hexwave~/hexwave~.c
+++ hexwave~/hexwave~.c
@@ -23,6 +23,7 @@ typedef struct _hexwave {
     double           half_height;
     double           zero_wait;
     double          samplerate;
+    short l_fcon;            // is a signal connected to the left inlet
 } t_hexwave;
 
 
@@ -48,7 +49,7 @@ void ext_main(void *r)
 	// unless you need to free allocated memory, in which case you should call dsp_free from
 	// your custom free function.
 
-	t_class *c = class_new("hexwave~", (method)hexwave_new, (method)hexwave_free, (long)sizeof(t_hexwave), 0L, A_GIMME, 0);
+	t_class *c = class_new("hexwave~", (method)hexwave_new, (method)dsp_free, (long)sizeof(t_hexwave), 0L, A_GIMME, 0);
 
     class_addmethod(c, (method)hexwave_float,               "float",     A_FLOAT, 0);
     class_addmethod(c, (method)hexwave_int,                 "int",     A_LONG, 0);
@@ -87,10 +88,9 @@ void *hexwave_new(t_symbol *s, long argc, t_atom *argv)
 }
 
 
-// NOT CALLED!, we use dsp_free for a generic free function
 void hexwave_free(t_hexwave *x)
 {
-	;
+    dsp_free((t_pxobject *)x);
 }
 
 
@@ -139,6 +139,7 @@ void hexwave_float(t_hexwave *x, double f)
 
 }
 
+
 void hexwave_int(t_hexwave *x, long f)
 {
     long in = proxy_getinlet((t_object *)x);
@@ -163,6 +164,8 @@ void hexwave_dsp64(t_hexwave *x, t_object *dsp64, short *count, double samplerat
 	// 5: flags to alter how the signal chain handles your object -- just pass 0
 	// 6: a generic pointer that you can use to pass any additional data to your perform method
 
+    post("count %i", count[0]);
+    x->l_fcon = count[0];    // signal connected to the frequency inlet?
     x->samplerate = samplerate;
 	object_method(dsp64, gensym("dsp_add64"), x, hexwave_perform64, 0, NULL);
 }
@@ -171,27 +174,33 @@ void hexwave_dsp64(t_hexwave *x, t_object *dsp64, short *count, double samplerat
 // this is the 64-bit perform method audio vectors
 void hexwave_perform64(t_hexwave *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
 {
-	//t_double *inL = ins[0];		// we get audio for each inlet of the object from the **ins argument
-	//t_double *outL = outs[0];	// we get audio for each outlet of the object from the **outs argument
-	int n = sampleframes;
+    t_double *in = ins[0];        // we get audio for each inlet of the object from the **ins argument
     t_double *out = outs[0];
+
+    int n = sampleframes;
     
+    //post("in: %i %f", x->l_fcon, in[0]);
+    //post("sample rate: %f", (float)x->samplerate);
+    //post("samples: %i", sampleframes);
+    
+    hexwave_change(&x->wave, x->reflect, x->peak_time, x->half_height, x->zero_wait);
+    double_t volscale = 1.0 - fabs(x->half_height / 4);
+
     t_float samples[sampleframes];
     
     for(int i=0; i < n; i++) {
         samples[i] = 0.0f;
     }
 
-    //post("sample rate: %f", (float)x->samplerate);
-    //post("samples: %i", sampleframes);
-    
-    hexwave_change(&x->wave, x->reflect, x->peak_time, x->half_height, x->zero_wait);
     hexwave_generate_samples(samples, n, &x->wave, x->freq / x->samplerate);
     
-    double_t volscale = 1.0 - fabs(x->half_height / 4);
-    
     for(int i=0; i < n; i++) {
-        out[i] = (double)samples[i] * volscale;
+        if(x->l_fcon) {
+            out[i] = (double)samples[i] * (1.0f - in[i]) * volscale;
+        }
+        else {
+            out[i] = (double)samples[i] * volscale;
+        }
     }
     
 }
